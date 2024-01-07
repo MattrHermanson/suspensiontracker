e560390bf0867034606c1b3e2d8e157c07311ba0
MattrHermanson <matth2004@outlook.com>
2024-01-06 22:10:03 -0800

    added contact and feedback page

--- common/urls.py
+++ common/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'common'
 urlpatterns = [
-    path('', views.home, name='common-home')
+    path('', views.home, name='common-home'),
+    path('contact/', views.contact, name='common-contact'),
+    path('feedback/', views.feedback, name='common-feedback'),
 ]
--- common/views.py
+++ common/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from users.models import User
+from django.core.mail import send_mail
 
 def home(request):
 
@@ -14,3 +15,31 @@ def home(request):
 
     return render(request, 'common/home.html')
 
+
+def contact(request):
+
+    if request.method == 'POST':
+
+        fname = request.POST['firstname']
+        lname = request.POST['lastname']
+        subject = request.POST['subject']
+        msg = request.POST['msg']
+
+        
+        send_mail(f'[Suspension Tracker Contact] ({fname} {lname}) {subject}', msg, '', ['matth2004@outlook.com'], fail_silently=False)
+        return redirect('common:common-contact')
+
+    return render(request, 'common/contact.html')
+
+
+def feedback(request):
+
+    if request.method == 'POST':
+        
+        feedback_type = request.POST['feedback_type']
+        desc = request.POST['desc']
+
+        send_mail(f'[Suspension Tracker {feedback_type.capitalize()}]', desc, '', ['matth2004@outlook.com'], fail_silently=False)
+        return redirect('common:common-feedback')
+
+    return render(request, 'common/feedback.html')
